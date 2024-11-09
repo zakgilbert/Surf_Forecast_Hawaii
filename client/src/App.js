be6f90a6141c7e5f28709e66439fbd940540b51b
zakgilbert <gilbertz@hawaii.edu>
2024-11-08 15:05:30 -1000

    Fix padding in report table.

--- client/src/App.js
+++ client/src/App.js
@@ -30,27 +30,34 @@ import {
 
 function App() {
   const [stationId, setStationId] = useState("");
-  const [renderData, setRenderData] = useState([])
+  const [renderData, setRenderData] = useState([]);
 
   const contentData = [
     { name: "North West", station: "51101", id: 0, tag: "buoy" },
     { name: "Hanalei", station: "51208", id: 1, tag: "buoy" },
-    {name:"Hanalei", station: "1611683", id: 2, tag: "tide"},
+    { name: "Hanalei", station: "1611683", id: 2, tag: "tide" },
     { name: "North West", station: "51101", id: 3, tag: "power" },
-    { name: "Hanalei", station: "51208", id: 4, tag: "power" }
+    { name: "Hanalei", station: "51208", id: 4, tag: "power" },
   ];
 
   const handleGridCall = (item) => {
-    console.log("in handle grid call")
-    console.log(item.station)
-    if(item.tag === "buoy") {
-      return <StationInput id={item.station}  />;
+    console.log("in handle grid call");
+    console.log(item.station);
+    if (item.tag === "buoy") {
+      return <StationInput id={item.station} />;
     }
-    if(item.tag === "power") {
-      return <Power id={item.station}/>;
+    if (item.tag === "power") {
+      return <Power id={item.station} />;
     }
-    if(item.tag === "tide") {
-      return <Tide id={item.station} beginDate={20241106} endDate={20241109} timeZone={'LST'}/>;
+    if (item.tag === "tide") {
+      return (
+        <Tide
+          id={item.station}
+          beginDate={20241106}
+          endDate={20241109}
+          timeZone={"LST"}
+        />
+      );
     }
     return <></>;
   };
@@ -59,10 +66,10 @@ function App() {
     setStationId(e.target.value);
   };
   const handleItemClick = (item) => {
-    setRenderData((prevData) =>[...prevData, item])
+    setRenderData((prevData) => [...prevData, item]);
   };
   function renderStation() {
-    console.log("in renderStation")
+    console.log("in renderStation");
     if (stationId.length === 5) {
       return <StationInput id={stationId} />;
     }
@@ -116,35 +123,32 @@ function App() {
           </section>
         </Sidebar>
         <div>
-        <Container style={{width: '80%'}}>
-        <SidebarPusher fluid>
-          <SegmentGroup fluid container>
-            <Segment>Top</Segment>
-            <SegmentGroup horizontal fluid >
-              <Segment placeholder fluid>
-                <Grid stackable columns={2}>
-                  {renderData.map((item) => (
-                    <GridColumn key={item.id} textAlign="center" fluid>
-                      <Card fluid>
-                        <CardContent fluid>
-                        <CardHeader>
-                          {item.station}
-                        </CardHeader>
-                        <CardDescription>
-                          {handleGridCall(item)}
-                        </CardDescription>
-                        </CardContent>
-                      </Card>
-                    </GridColumn>
-                  ))}
-                </Grid>
-              </Segment>
-            </SegmentGroup>
-            <Segment>Bottom</Segment>
-          </SegmentGroup>
-        </SidebarPusher>
-
-        </Container>
+          <Container style={{ width: "80%" }}>
+            <SidebarPusher fluid>
+              <SegmentGroup fluid container>
+                <Segment>Top</Segment>
+                <SegmentGroup horizontal fluid>
+                  <Segment placeholder fluid>
+                    <Grid stackable columns={2}>
+                      {renderData.map((item) => (
+                        <GridColumn key={item.id} textAlign="center" fluid>
+                          <Card fluid>
+                            <CardContent fluid>
+                              <CardHeader>{item.station}</CardHeader>
+                              <CardDescription>
+                                {handleGridCall(item)}
+                              </CardDescription>
+                            </CardContent>
+                          </Card>
+                        </GridColumn>
+                      ))}
+                    </Grid>
+                  </Segment>
+                </SegmentGroup>
+                <Segment>Bottom</Segment>
+              </SegmentGroup>
+            </SidebarPusher>
+          </Container>
         </div>
       </Container>
     </div>
