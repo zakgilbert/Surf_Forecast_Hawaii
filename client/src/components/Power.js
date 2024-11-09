32711ef1b92c579cd0830fca8ffdd9b61fcd419f
zakgilbert <gilbertz@hawaii.edu>
2024-11-08 17:25:00 -1000

    Format data in buoy table.

--- client/src/components/Power.js
+++ client/src/components/Power.js
@@ -1,59 +1,96 @@
-import React, {useState, useEffect} from "react";
-import { Container, Header, Button, Icon, Menu, Popup, Table, TableCell } from 'semantic-ui-react'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Label } from 'recharts';
-import moment from 'moment'; // Import moment.js for date formatting
-const Power = ({id}) => {
-    const [data, setData] = useState([{}])
-    
-    useEffect(() => {
-        fetch(`/power/${id}`).then(
-            res => res.json()
-        ).then(
-            data => {
-               setData(data) 
-               console.log(data)
-            }
-        )
-    }, [id])
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Header,
+  Button,
+  Icon,
+  Menu,
+  Popup,
+  Table,
+  TableCell,
+} from "semantic-ui-react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  Label,
+} from "recharts";
+import moment from "moment"; // Import moment.js for date formatting
 
-    return (((data !== undefined) ) ? (
-        <Container textAlign="center">
-            <Header as="h2" color="blue">Swell Power</Header>
-            <ResponsiveContainer width="100%" height={360}>
-                <LineChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis 
-                        dataKey="dataTime" 
-                        tickFormatter={(timeStr) => 
-                            moment(timeStr, 'YYYY-MM-DD hh:mm A').format('MMM D, h:mm A')} // Format for x-axis
-                    />
-                     <YAxis
-                     label={{ value: 'Energy Value', angle: -90, position: 'insideLeft' }} 
-                     />
-                    <Tooltip 
-                        formatter={(value, name, props) => {
-                            const dateTime = moment(props.payload.dataTime, 'YYYY-MM-DD hh:mm A'); // Specify format
-                            if (!dateTime.isValid()) {
-                                console.error('Invalid date:', props.payload.dataTime);
-                                return ['Invalid value', 'Invalid date'];
-                            }
-                            return [`Value: ${value}`]; // Return formatted value and dateTime
-                        }}
-                        labelFormatter={(label) => {
-                            const dateTime = moment(label, 'YYYY-MM-DD hh:mm A'); // Specify format
-                            if (!dateTime.isValid()) {
-                                console.error('Invalid label date:', label);
-                                return 'Invalid date';
-                            }
-                            return `Date: ${dateTime.format('MMM D, YYYY h:mm A')}`; // Format the label
-                        }} 
-                    />
-                    <Legend />
-                    <Line type="natural" dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
-                </LineChart>
-            </ResponsiveContainer>
-        </Container>
-    ) : (<></>))
+const Power = ({ id }) => {
+  const [data, setData] = useState([{}]);
 
-}
-export default Power
+  useEffect(() => {
+    fetch(`/power/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, [id]);
+
+  return data !== undefined ? (
+    <Container textAlign="center">
+      <Header as="h2" color="blue">
+        Swell Power
+      </Header>
+      <ResponsiveContainer width="100%" height={360}>
+        <LineChart
+          data={data}
+          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="dataTime"
+            tickFormatter={(timeStr) =>
+              moment(timeStr, "YYYY-MM-DD hh:mm A").format("MMM D, h:mm A")
+            } // Format for x-axis
+          />
+          <YAxis
+            label={{
+              value: "Energy Value",
+              angle: -90,
+              position: "insideLeft",
+            }}
+          />
+          <Tooltip
+            formatter={(value, name, props) => {
+              const dateTime = moment(
+                props.payload.dataTime,
+                "YYYY-MM-DD hh:mm A"
+              ); // Specify format
+              if (!dateTime.isValid()) {
+                console.error("Invalid date:", props.payload.dataTime);
+                return ["Invalid value", "Invalid date"];
+              }
+              return [`Value: ${value}`]; // Return formatted value and dateTime
+            }}
+            labelFormatter={(label) => {
+              const dateTime = moment(label, "YYYY-MM-DD hh:mm A"); // Specify format
+              if (!dateTime.isValid()) {
+                console.error("Invalid label date:", label);
+                return "Invalid date";
+              }
+              return `Date: ${dateTime.format("MMM D, YYYY h:mm A")}`; // Format the label
+            }}
+          />
+          <Legend />
+          <Line
+            type="natural"
+            dataKey="value"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </Container>
+  ) : (
+    <></>
+  );
+};
+export default Power;
--- client/src/components/StationInput.js
+++ client/src/components/StationInput.js
@@ -1,52 +1,61 @@
-
-import React, { useState, useEffect } from "react"
-import { Icon, Label, Menu, Popup, Table, TableCell } from 'semantic-ui-react'
+import React, { useState, useEffect } from "react";
+import { Icon, Label, Menu, Popup, Table, TableCell } from "semantic-ui-react";
 
 const StationInput = ({ id }) => {
-    const [data, setData] = useState([{}])
+  const [data, setData] = useState([{}]);
 
+  const formatDate = (datetime) => {
+    const date = new Date(datetime);
+    return date.toLocaleString("en-US", {
+      month: "short", // MMM
+      day: "numeric", // D
+      hour: "numeric", // h
+      minute: "numeric", // mm
+      hour12: true, // A (AM/PM)
+    });
+  };
+  useEffect(() => {
+    fetch(`/report/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, [id]);
 
-    useEffect(() => {
-        fetch(`/report/${id}`).then(
-            res => res.json()
-        ).then(
-            data => {
-                setData(data)
-                console.log(data)
-            }
-        )
-    }, [id])
-
-    return (((data.cols !== undefined) && (data.rows !== undefined)) ? (
-        <div style={{ maxHeight: '400px', overflowY: 'auto' }}> {/* Adjust height as needed */}
-        <Table celled>
-            <Table.Header>
-                <Table.Row>
-                    {
-                        data.cols.map((col) =>
-                            <Popup trigger=
-                                {
-                                    <Table.HeaderCell key={col} >{col.value}</Table.HeaderCell>
-                                }>
-                                {col.toolTip}
-                            </Popup>)
-                    }
-                </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                {
-                    data.rows.map((row) =>
-                        <Table.Row key={row}>
-                            {
-                                row.map((val) =>
-                                    <TableCell key={val} style={{padding: '5px'}}>{val}</TableCell>)
-                            }
-                        </Table.Row>
-                    )
+  return data.cols !== undefined && data.rows !== undefined ? (
+    <div style={{ maxHeight: "400px", overflowY: "auto" }}>
+      {" "}
+      {/* Adjust height as needed */}
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            {data.cols.map((col) => (
+              <Popup
+                trigger={
+                  <Table.HeaderCell key={col}>{col.value}</Table.HeaderCell>
                 }
-            </Table.Body>
-        </Table>
-        </div>
-    ) : (<></>))
-}
-export default StationInput
+              >
+                {col.toolTip}
+              </Popup>
+            ))}
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {data.rows.map((row, rowIndex) => (
+            <Table.Row key={rowIndex}>
+              {row.map((val, colIndex) => (
+                <TableCell key={colIndex} style={{ padding: "5px" }}>
+                  {data.cols[colIndex].value === "Time" ? formatDate(val) : val}
+                </TableCell>
+              ))}
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </div>
+  ) : (
+    <></>
+  );
+};
+export default StationInput;
