038168c7438604f6c3265d99120f48b44070b7d3
zakgilbert <gilbertz@hawaii.edu>
2026-04-09 15:25:25 -1000

    Migrate CSS for DesktopApp.js.

--- client/src/components/DesktopApp.js
+++ client/src/components/DesktopApp.js
@@ -1,10 +1,4 @@
-/**
- * DesktopApp.js
- */
-import React, { useState } from "react";
-import StationInput from "./StationInput.js";
-import Power from "./Power.js";
-import Tide from "./Tide.js";
+import { useState } from "react";
 import { handleGridCall, groupedData } from "../utility.js";
 
 import {
@@ -13,17 +7,12 @@ import {
   Grid,
   Divider,
   GridColumn,
-  GridRow,
-  Header,
-  Icon,
-  Search,
   Button,
   Sidebar,
   Menu,
   SidebarPusher,
   Container,
   List,
-  ListItem,
   Card,
   CardContent,
   CardHeader,
@@ -42,12 +31,13 @@ function App() {
   const handleChange = (e) => {
     setStationId(e.target.value);
   };
+
   const handleItemClick = (item) => {
     setRenderData((prevData) => [...prevData, item]);
   };
 
   return (
-    <div>
+    <div className="desktop-app">
       <Container fluid>
         <Sidebar
           as={Menu}
@@ -57,38 +47,35 @@ function App() {
           vertical
           visible
           direction="left"
+          className="desktop-app-sidebar"
         >
-          <section className="garamond">
-            <div className="navy georgia ma0 grow">
+          <section className="garamond desktop-app-sidebar-section">
+            <div className="navy georgia ma0 grow desktop-app-sidebar-title-wrap">
               <h2 className="f2"></h2>
             </div>
-            <div className="pa2">
+
+            <div className="desktop-app-search-wrap">
               <input
-                className="pa3 bb br3 grow b--none bg-lightest-blue ma3"
+                className="pa3 bb br3 grow b--none bg-lightest-blue ma3 desktop-app-search-input"
                 type="search"
                 placeholder="Search Buoy"
                 onChange={handleChange}
               />
             </div>
+
             <div>
               {Object.keys(groupedData).map((tag) => (
-                <div
-                  key={tag}
-                  style={{
-                    textAlign: "left",
-                    color: "white",
-                    marginTop: "20px",
-                    marginBottom: "5px",
-                    marginLeft: "10px",
-                  }}
-                >
-                  <h3>{tag.charAt(0).toUpperCase() + tag.slice(1)}</h3>
-                  <List link direction="left">
+                <div key={tag} className="desktop-app-group">
+                  <h3 className="desktop-app-group-title">
+                    {tag.charAt(0).toUpperCase() + tag.slice(1)}
+                  </h3>
+
+                  <List link direction="left" className="desktop-app-list">
                     {groupedData[tag].map((data) => (
-                      <List.Item key={data.id}>
+                      <List.Item key={data.id} className="desktop-app-list-item">
                         <a
                           onClick={() => handleItemClick(data)}
-                          style={{ display: "block", textAlign: "left" }}
+                          className="desktop-app-list-link"
                         >
                           {data.name}
                         </a>
@@ -101,34 +88,22 @@ function App() {
           </section>
         </Sidebar>
 
-        {/* Conditional rendering for the Refresh button and grid */}
         {renderData.length > 0 && (
-          <div>
+          <div className="desktop-app-content-shell">
             <Button
               onClick={clearGrid}
               color="red"
-              style={{
-                position: "absolute",
-                top: "10px",
-                right: "10px",
-                zIndex: 1,
-              }}
+              className="desktop-app-refresh-button"
             >
               Refresh
             </Button>
-            <div
-              style={{
-                padding: "1rem",
-                width: "100%",
-                maxWidth: "1600px",
-                margin: "0 auto",
-              }}
-            >
-              <SidebarPusher fluid>
-                <SegmentGroup fluid container>
-                  <SegmentGroup horizontal fluid>
-                    <Divider></Divider>
-                    <Segment placeholder fluid>
+
+            <div className="desktop-app-content-wrap">
+              <SidebarPusher className="desktop-app-pusher">
+                <SegmentGroup className="desktop-app-segment-group">
+                  <SegmentGroup horizontal className="desktop-app-segment-group-horizontal">
+                    <Divider />
+                    <Segment placeholder className="desktop-app-main-segment">
                       <Grid stackable doubling columns={2}>
                         {renderData.map((item) => (
                           <GridColumn
@@ -138,25 +113,11 @@ function App() {
                             computer={8}
                             textAlign="center"
                           >
-                            <Card
-                              fluid
-                              style={{
-                                width: "100%",
-                                display: "flex",
-                                minHeight: "725px",
-                                flexDirection: "column",
-                              }}
-                            >
-                              <CardContent
-                                style={{
-                                  flex: "1",
-                                  display: "flex",
-                                  flexDirection: "column",
-                                }}
-                              >
+                            <Card fluid className="desktop-app-card">
+                              <CardContent className="desktop-app-card-content">
                                 <CardHeader>{item.header}</CardHeader>
                                 <CardMeta>{item.meta}</CardMeta>
-                                <CardDescription style={{ overflowY: "visible" }}>
+                                <CardDescription className="desktop-app-card-description">
                                   {handleGridCall(item)}
                                 </CardDescription>
                               </CardContent>
@@ -175,4 +136,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
