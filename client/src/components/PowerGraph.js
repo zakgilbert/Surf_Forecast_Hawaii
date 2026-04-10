ab018e7b552ca5063d7534da99ce0afe747b0ff2
zakgilbert <gilbertz@hawaii.edu>
2026-04-09 16:40:28 -1000

    Migrate CSS for PowerGraph.js.

--- client/src/components/PowerGraph.js
+++ client/src/components/PowerGraph.js
@@ -1,31 +1,52 @@
-import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React from "react";
+import { Container, Header } from "semantic-ui-react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
-// Sample data
 const data = [
-  { name: 'Jan', value: 40 },
-  { name: 'Feb', value: 30 },
-  { name: 'Mar', value: 20 },
-  { name: 'Apr', value: 27 },
-  { name: 'May', value: 18 },
-  { name: 'Jun', value: 23 },
+  { name: "Jan", value: 40 },
+  { name: "Feb", value: 30 },
+  { name: "Mar", value: 20 },
+  { name: "Apr", value: 27 },
+  { name: "May", value: 18 },
+  { name: "Jun", value: 23 },
 ];
 
-const Graph = () => (
-  <Container textAlign="center">
-    <Header as="h2" color="blue">Monthly Data Overview</Header>
-    <ResponsiveContainer width="100%" height={400}>
-      <LineChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
-      </LineChart>
-    </ResponsiveContainer>
+const PowerGraph = () => (
+  <Container textAlign="center" className="power-graph-container">
+    <Header as="h2" className="power-graph-title">
+      Monthly Data Overview
+    </Header>
+
+    <div className="power-graph-chart-wrap">
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart
+          data={data}
+          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="value"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   </Container>
 );
 
-export default Graph;
+export default PowerGraph;
